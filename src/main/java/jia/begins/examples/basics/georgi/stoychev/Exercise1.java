ad0a1bebe322ebf84fa3f2e45543c41d1965e2ba
georgi <georgi@linux-p7e3.site>
2010-11-21 20:04:41 +0200

    exercise1

---
+++ src/main/java/jia/begins/examples/basics/georgi/stoychev/Exercise1.java
@@ -0,0 +1,11 @@
+package jia.begins.examples.basics.georgi.stoychev;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: georgi
+ * Date: Nov 21, 2010
+ * Time: 5:21:57 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Exercise1 {
+}
